a2ac7be87d04ce97de2308a783f8a6da2a7e8c43
Gagandeep Jhanji <103517339+gagankj@users.noreply.github.com>
2022-07-21 11:47:11 +0530

    Update main (9).py

--- main (9).py
+++ main (9).py
@@ -1,9 +1,4 @@
-
-for num in range(1800,3000):
-    if(num%8==0):
-        if(num%6==0):
-            print(".")
-        else:
-            print(num)
+for num in range(1800, 3000):
+    if num%4 ==0 and num%8 == 0 and num%6 != 0:
+        print(num)
         
-    
